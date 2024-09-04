0f6eb11e57fd3f00ab1ed12a9650e0ac65ee9fff
Kyumin-Chang <kyumin0529@naver.com>
2024-09-04 20:01:37 +0900

    feat: init

---
+++ src/main/java/com/golang/golang/GolangApplication.java
@@ -0,0 +1,13 @@
+package com.golang.golang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/golang/golang/GolangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golang.golang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
