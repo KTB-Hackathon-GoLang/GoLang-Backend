5cbe6d018dc5b399171f9221d6bd65f24d4a66bb
박지혁 <pjh5365@naver.com>
2024-09-07 02:07:12 +0900

    Comment: 주석 제거

--- src/main/java/golang/chat/repository/ChatDetailRepository.java
+++ src/main/java/golang/chat/repository/ChatDetailRepository.java
@@ -9,9 +9,6 @@ import golang.chat.domain.entity.ChatDetail;
 
 /**
  * 채팅방 상황설명 Repository
- *
- * @author : parkjihyeok
- * @since : 2024/09/06
  */
 public interface ChatDetailRepository extends JpaRepository<ChatDetail, Long> {
 
