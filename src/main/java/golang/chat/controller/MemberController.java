7481f9ef65c43c6afbc086368e7d9643b819aa2b
박지혁 <pjh5365@naver.com>
2024-09-06 22:02:46 +0900

    Fix: 로그인 요청이 들어오면 항상 새로운 회원을 만들며 회원아이디를 리턴하도록 수정함

--- src/main/java/golang/chat/controller/MemberController.java
+++ src/main/java/golang/chat/controller/MemberController.java
@@ -1,50 +1,25 @@
 package golang.chat.controller;
 
-import golang.chat.domain.dto.response.ApiResponse;
-import golang.chat.domain.dto.response.MemberLoginResponse;
-import golang.chat.service.MemberService;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import golang.chat.domain.dto.response.ApiResponse;
+import golang.chat.service.MemberService;
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class MemberController {
 
-    private final MemberService memberService;
+	private final MemberService memberService;
 
-    @PostMapping("/login")
-    public ResponseEntity<?> login(HttpServletRequest request, HttpServletResponse response) {
-        String username = null;
-        Cookie[] cookies = request.getCookies();
-
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("username")) {
-                    username = cookie.getValue();
-                    break;
-                }
-            }
-        }
-
-        if (username == null || username.isEmpty()) {
-            MemberLoginResponse newMember = memberService.saveRandomMember();
-            Cookie usernameCookie = new Cookie("username", newMember.getUsername());
-            // usernameCookie.setHttpOnly(true);
-            response.addCookie(usernameCookie);
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(ApiResponse.success("로그인 하였습니다.", newMember));
-        }
-
-        return ResponseEntity.status(HttpStatus.FOUND)
-                .header("Location", "/already-logged-in") // 리다이렉트 URL
-                .build();
-    }
+	@PostMapping("/login")
+	public ResponseEntity<?> login() {
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(ApiResponse.success("로그인 하였습니다.", memberService.saveRandomMember()));
+	}
 }
