13e967996b4e7fe0610439d3aff3840c278f30d3
박지혁 <pjh5365@naver.com>
2024-09-06 21:58:56 +0900

    Fix: 프론트에서 쿠키 접근 허용

--- src/main/java/golang/chat/controller/MemberController.java
+++ src/main/java/golang/chat/controller/MemberController.java
@@ -37,7 +37,7 @@ public class MemberController {
         if (username == null || username.isEmpty()) {
             MemberLoginResponse newMember = memberService.saveRandomMember();
             Cookie usernameCookie = new Cookie("username", newMember.getUsername());
-            usernameCookie.setHttpOnly(true);
+            // usernameCookie.setHttpOnly(true);
             response.addCookie(usernameCookie);
             return ResponseEntity.status(HttpStatus.OK)
                     .body(ApiResponse.success("로그인 하였습니다.", newMember));
