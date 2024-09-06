2e6170d825b5a5db327f868ad16f7ad6c5276784
박지혁 <pjh5365@naver.com>
2024-09-06 12:56:14 +0900

    Test: 필요없는 내용 삭제

--- src/test/java/golang/GolangApplicationTests.java
+++ src/test/java/golang/GolangApplicationTests.java
@@ -3,10 +3,8 @@ package golang;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @Disabled
-@ActiveProfiles("test")
 @SpringBootTest
 class GolangApplicationTests {
 
