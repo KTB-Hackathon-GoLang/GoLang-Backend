94b9e7cb1d9d9e07ffc4d21b4529012fa4818467
박지혁 <pjh5365@naver.com>
2024-09-07 12:29:09 +0900

    Fix: 직렬화 수정

--- src/main/java/golang/chat/domain/dto/response/AIResponse.java
+++ src/main/java/golang/chat/domain/dto/response/AIResponse.java
@@ -2,15 +2,26 @@ package golang.chat.domain.dto.response;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * AI 필터링 결과
  */
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class AIResponse {
 
 	private List<String> messages;
+
+
+	@JsonCreator
+	public AIResponse(@JsonProperty("messages") List<String> messages) {
+		this.messages = messages;
+	}
+
 }
